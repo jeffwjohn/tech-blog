34242982f9bc56384d3ac59e022c8127eb7d7026
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-30 14:26:12 -0500

    add README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # tech-blog
 ![](https://img.shields.io/badge/License-MIT-blue.svg)
 ## Description 
-Note taker is an interactive, Express.js-driven application that captures user input in the form of notes stored on a backend server. The notes are each assigned a unique ID, and are editable and deletable at the push of a button.
+Tech Blog is an interactive, express.js, sequelize and mySql-driven application that captures user input in the form of blogs stored on a backend server. The blogs are each assigned a unique ID, and may be edited and deleted at the push of a button. Users may login to the server and upvote and comment on individual blog posts. 
 
-## Deployed Application Link: https://fierce-anchorage-07294.herokuapp.com/
-![Screenshot](public/assets/images/screenshot.png)
+## Deployed Application Link: https://afternoon-cove-12974.herokuapp.com/
+![Screenshot](public/images/screenshot.png)
 
 ## Table of Contents
 * [Installation](#installation) 
@@ -15,13 +15,23 @@ Note taker is an interactive, Express.js-driven application that captures user i
 
  
 ### Installation
-  Clone or fork the code from the Team-Profile-Generator repository on GitHub. Open the code in a standard code editing application, such as VS Code.
+  Clone or fork the code from the Tech-Blog repository on GitHub. Open the code in a standard code editing application, such as VS Code. Install npm and the following dependencies: 
+  * bcrypt
+  * connect-session-sequelize
+  * dotenv
+  * express
+  * express-handlebars
+  * express-session
+  * jquery
+  * mysql2
+  * sequelize
+  * jest
 ### Usage
- Open the deployed application via Heroku with the following link. On the landing page, click "Get Started" to open the "notes" page. Click on the empty title and note fields and write notes. Click the save button that appears in the upper-right corner to save your notes, as displayed in the left-hand column. Delete notes by clicking their corresponding trash can icons.
+ Open the deployed application via Heroku with the provided link above. On the home page, browse blog posts and comments freely. To create posts, comments, and upvotes, fill out the signup form to login. 
 ### License
   MIT
 ### Contributing
-Note-taker is an open source project, and anyone is encourged to contribute by cloning or forking the code and working to improve its function and versatility.
+Tech Blog is an open source project, and anyone is encouraged to contribute by cloning or forking the code and working to improve its function and versatility.
 
 ### Questions
     
--- public/javascript/auto-logout.js
+++ public/javascript/auto-logout.js
@@ -24,9 +24,9 @@ class AutoLogout {
   }
 
   setTimeout() {
-    // this.warnTimeout = setTimeout(this.warn, 1 * 10 * 1000);
+    this.warnTimeout = setTimeout(this.warn, 1 * 20 * 1000);
 
-    // this.logoutTimeout = setTimeout(this.logout, 1 * 20 * 1000);
+    this.logoutTimeout = setTimeout(this.logout, 1 * 30 * 1000);
   }
 
   resetTimeout() {
@@ -67,7 +67,7 @@ class AutoLogout {
 
     console.log('Sending a logout request to the API...');
     alert('You have been automatically logged out due to inactivity!')
-    // logout();
+    logout();
     this.destroy(); // Cleanup
   }
 
@@ -80,115 +80,5 @@ class AutoLogout {
   }
 }
 
-// Set timeout variables.
-// var timeoutWarning = 840000; // Display warning in 14 Mins.
-// var timeoutNow = 60000; // Warning has been shown, give the user 1 minute to interact
-// var logoutUrl = 'logout.php'; // URL to logout page.
-
-// var warningTimer;
-// var timeoutTimer;
-
-// // Start warning timer.
-// function StartWarningTimer() {
-//     warningTimer = setTimeout("IdleWarning()", timeoutWarning);
-// }
-
-// // Reset timers.
-// function ResetTimeOutTimer() {
-//     clearTimeout(timeoutTimer);
-//     StartWarningTimer();
-//     $("#timeout").dialog('close');
-// }
-
-// // Show idle timeout warning dialog.
-// function IdleWarning() {
-//     clearTimeout(warningTimer);
-//     timeoutTimer = setTimeout("IdleTimeout()", timeoutNow);
-//     $("#timeout").dialog({
-//         modal: true
-//     });
-// Add code in the #timeout element to call ResetTimeOutTimer() if
-// the "Stay Logged In" button is clicked
-// }
-
-// Logout the user.
-// function IdleTimeout() {
-//     window.location = logoutUrl;
-// }
-
-// var AutoLogout = (function() {
-//   function AutoLogout() {
-//     this.events = ['load', 'mousemove', 'mousedown',
-//                    'click', 'scroll', 'keypress'];
-
-//     this.warn = this.warn.bind(this);
-//     this.logout = this.logout.bind(this);
-//     this.resetTimeout = this.resetTimeout.bind(this);
-
-//     var self = this;
-//     this.events.forEach(function(event) {
-//       window.addEventListener(event, self.resetTimeout);
-//     });
-
-//     this.setTimeout();
-//   }
-
-//   var _p = AutoLogout.prototype;
-
-//   _p.clearTimeout = function() {
-//     if(this.warnTimeout)
-//       clearTimeout(this.warnTimeout);
-
-//     if(this.logoutTimeout)
-//       clearTimeout(this.logoutTimeout);
-//   };
-
-//   _p.setTimeout = function() {
-//     this.warnTimeout = setTimeout(this.warn, 1 * 5 * 1000);
-
-//     this.logoutTimeout = setTimeout(this.logout, 1 * 10 * 1000);
-//   };
-
-//   _p.resetTimeout = function() {
-//     this.clearTimeout();
-//     this.setTimeout();
-//   };
-
-//   _p.warn = function() {
-//     alert('You will be logged out automatically in 1 minute.');
-//   };
-
-//   _p.logout = function() {
-//     // Send a logout request to the API
-//     console.log('Sending a logout request to the API...');
-
-//     this.destroy();  // Cleanup
-//   };
-
-//   _p.destroy = function() {
-//     this.clearTimeout();
-
-//     var self = this;
-//     this.forEach(function(event) {
-//       window.removeEventListener(event, self.resetTimeout);
-//     });
-//   };
-
-//   return AutoLogout;
-// })();
-
-// module.exports = AutoLogout;
-
-// var myVar;
-
-// function openWin() {
-//   var myWindow = window.open("", "myWindow", "width=200, height=100");
-//   myWindow.document.write("<p>This is 'myWindow'</p>");
-//   setTimeout(function(){ myWindow.close() }, 3000);
-// }
-
 window.onload = new AutoLogout();
 
-// function alertFunc() {
-//   alert("Hello!");
-// }
